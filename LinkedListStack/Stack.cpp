50c3bbdbc05b1a9ebaef3daa5c9df7e895095190
not_sure <avfinogenov@gmail.com>
2020-07-10 12:03:01 +0300

    stack v1

--- LinkedListStack/Stack.cpp
+++ LinkedListStack/Stack.cpp
@@ -1,1 +1,113 @@
 #include "Stack.h"
+
+Stack::Stack()
+{
+}
+
+Stack::Stack(Stack& source)
+{
+	count = source.count;
+	Linklist* tmp;
+	tmp = source.plast;
+	int* t = new int[count];
+	for(int i=0;i<count;i++)
+	{
+		t[i] = tmp->val;
+		tmp = tmp->pprev;
+		//node = source.plast->pprev;
+	}
+	for (int i = count ; i >0 ; i--)
+	{
+		Push(t[i - 1]);
+	}
+	delete[] t;
+
+}
+
+Stack& Stack::operator=(Stack& source)
+{
+	
+	for (int i = 0; i < source.count; i++)
+	{
+		Pop();
+	}
+	count = source.count;
+	Linklist* tmp;
+	tmp = source.plast;
+	int* t = new int[count];
+	for (int i = 0; i < count; i++)
+	{
+		t[i] = tmp->val;
+		tmp = tmp->pprev;
+		//node = source.plast->pprev;
+	}
+	for (int i = count; i > 0; i--)
+	{
+		this->Push(t[i - 1]);
+	}
+	delete[] t;
+
+	return *this;
+
+
+	// TODO: вставьте здесь оператор return
+}
+
+Stack::~Stack()
+{
+	while(plast!=nullptr)
+		{
+			Pop();
+		}
+	
+}
+
+void Stack::Push(int in_val)
+{
+	Linklist* node = new Linklist(in_val);
+	if (plast == nullptr )
+	{
+		plast = node;
+		
+	}
+	else
+	{
+		node->pprev = plast;
+		plast = node;
+		
+	}
+	count++;
+}
+
+int Stack::Pop()
+{
+	if (plast != nullptr)
+	{
+		Linklist* p = plast->pprev;
+		int tmp = plast->val;
+		delete plast;
+		plast = p;
+		count--;
+		return tmp;
+		
+	}
+	else
+	{
+		return -1;
+	}
+}
+
+int Stack::Size() const
+{
+
+	return count;
+}
+
+bool Stack::Empty() const
+{
+	if (plast==nullptr)
+	{
+		return true;
+	}
+	return false;
+}
--- LinkedListStack/Stack.h
+++ LinkedListStack/Stack.h
@@ -1,10 +1,38 @@
 #pragma once
-
+class Linklist
+{
+	
+	
+public:
+	int val;
+	Linklist* pprev = nullptr;
+	Linklist();
+	Linklist(int in_val)
+	{
+		val = in_val;
+	}
+	Linklist& operator =(Linklist& source)
+	{
+		val = source.val;
+	}
+};
 class Stack
 {
+
+	
+
 public:
-	void Push( int val );
+	Stack();
+	Stack(Stack& source);
+	Stack& operator= (Stack& source);
+	~Stack();
+	void Push( int in_val);
 	int Pop();
 	int Size() const;
 	bool Empty() const;
+	Linklist* plast = nullptr;
+private:
+	int count = 0;
+	
+	
 };
